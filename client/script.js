11c08f340fc3fb6cb9bc10e1dbc33bcf1a3844d8
Anvilgit <slowmovingsnail@gmail.com>
2023-04-02 13:38:28 +0300

    update the server url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);  // messageDiv.innerHTML = "..."
 
   //fetch data from server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-ummj.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
